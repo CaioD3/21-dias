9b27336c73a346d91d8f190a7816ca3061739d41
Caio Dantas <caio.dantas.2004@gmail.com>
2024-06-24 20:47:11 -0300

    adicionando arquivo dia 3

---
+++ dia_03/index.js
@@ -0,0 +1,10 @@
+let Nome = prompt ("insira seu nome");
+let Idade = Number(prompt ("insira sua idade"));
+let Altura = Number(prompt ("insira sua altura"));
+let Peso= Number(prompt ("insira sua peso"));
+
+Nascimento = (2023 - Idade)
+IMC = (Peso/ (Altura * Altura))
+
+
+console.log (`Ola ${Nome}, voce tem ${Idade}, nasceu em ${Nascimento}, tem ${Altura} , pesa ${Peso}, seu IMC é ${IMC} Kg/m2 `)
